41956f64a4481de6faba0783b83c08d037e5d417
doowon13 <83566946+doowon13@users.noreply.github.com>
2024-03-07 17:49:51 +0900

    Petmily Final commit
    Petmily Final commit

--- src/main/java/like/exception/LikeException.java
+++
@@ -1,7 +0,0 @@
-package like.exception;
-
-public class LikeException extends Exception{
-	public LikeException (String message) {
-		super(message);
-	}
-}
--- src/main/java/reply/exception/ReplyException.java
+++
@@ -1,7 +0,0 @@
-package reply.exception;
-
-public class ReplyException extends Exception{
-	public ReplyException(String message) {
-		super(message);
-	}
-}
